5263ee4c6c84fec4d2ecd046e5785493a67e85e1
CableX88 <Nullspace101@gmail.com>
2017-08-31 01:20:30 -0700

    fizzbuzz
    modified fizzbuzz program for more clarity

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,10 +1,10 @@
 #My solution for the fizz buzz interview question
 for num in range(1,101):
-    message = ''
+    message = '' # creates an empty string
     if num % 3 == 0:
         message += 'Fizz'
-    if num % 5 == 0:       # no more elif
+    if num % 5 == 0:       
         message += 'Buzz'
-    if not message:      # check if msg is an empty string
+    if not message:      # check if message is an empty string
         message += str(num)
     print (message)
